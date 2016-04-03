0092aa25d5cfd66fbda2f1a0cbf449b05b484f1d
higore <kjhihing@gmail.com>
2016-04-03 22:39:43 +0900

    튜토리얼

---
+++ tutorial/angular/app.js
@@ -0,0 +1,13 @@
+(function(){
+  var app = angular.module('store', []);
+  var gem = {
+    name : 'Dodecahedron'
+    ,price : 2.95
+    ,description : "this is my first test"
+  }
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+  alert(1);
+  
+})();
