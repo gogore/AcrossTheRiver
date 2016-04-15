efad8d864a1bfbcb4213d35c53611d94e0e50587
higore <kjhihing@gmail.com>
2016-04-15 15:04:47 +0900

    앵귤러 테스트

--- tutorial/angular/app.js
+++ tutorial/angular/app.js
@@ -1,11 +1,11 @@
-(function(){
-  var app = angular.module('store', []);
-  var gem = {
-    name : 'Dodecahedron'
-    ,price : 2.95
-    ,description : "this is my first test"
-  }
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
+(function() {
+	var app = angular.module('store', []);
+	var gem = {
+		name : 'Dodecahedron',
+		price : 1000000000000,
+		description : "this is my first test"
+	}
+	app.controller('StoreController', function($scope) {
+		$scope.product = gem;
+	});
 })();
